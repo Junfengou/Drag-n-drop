04870e57f808e5340581e4595b4864f2dadf1fa8
Junfeng Ou <jun@AleesAppleWatch.localdomain>
2022-05-04 10:33:24 -0500

    Going to add a new package for tree structure

--- assets/data.js
+++ assets/data.js
@@ -1,18 +1,76 @@
 export const initialColumns = {
     Attribute: {
       id: 'Attribute',
-      list: ['Tenure', 'Location', 'Manager Levels', 'Region', 'Generation', 'Squad', 'Role'],
+      list: [
+        'Tenures',
+        'Locations', 
+        'Manager Levels', 
+        'Region', 
+        'Generation', 
+        'Squad', 
+        'Role'],
     },
     OrgTree: {
       id: 'OrgTree',
       list: [],
     },
-    // Preview: {
-    //   id: 'Preview',
-    //   list: []
-    // }
   }
 
-  // const location = {
-  //   name: ''
-  // }
+  /*
+      Attribute: {
+      id: 'Attribute',
+      list: [
+        {
+          colName: 'Tenures', 
+          colData: tenures
+        },
+        'Locations', 
+        'Manager Levels', 
+        'Region', 
+        'Generation', 
+        'Squad', 
+        'Role'],
+    },
+    OrgTree: {
+      id: 'OrgTree',
+      list: [],
+    },
+  */
+
+  const tenures = [
+    {
+      name: '1 - 2 years'
+    },
+    {
+      name: '3 - 5 years'
+    },
+    {
+      name: '6 - 9 years'
+    },
+    {
+      name: 'Less than 1 year'
+    }
+  ]
+
+  const locations = [
+    {
+      name: 'Boston',
+      data: ['Executive', 'Finance', 'HR', 'Information Technology', 'Operations', 'Sales']
+    },
+    {
+      name: 'Dallas',
+      data: ['Executive', 'Information Technology', 'Operations', 'Sales']
+    },
+    {
+      name: 'Des Moines',
+      data: ['Executive', 'Sales']
+    },
+    {
+      name: 'San Diego',
+      data: ['Operations', 'Sales']
+    },
+    {
+      name: 'San Franciso',
+      data: ['Information Technology', 'Operations', 'Sales']
+    },
+  ]
